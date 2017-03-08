28a09ffc2beebb21a7ea45867834eab4550cc145
Saurabh Sood <saurabhsood91@gmail.com>
2017-03-07 17:38:45 -0800

    initial commit

---
+++ lib/github.py
@@ -0,0 +1,15 @@
+import requests
+
+class Github:
+    @staticmethod
+    def get_jobs(query, location=None):
+        url = 'https://jobs.github.com/positions.json'
+
+        params = {
+            'description': query,
+            'location': location
+        }
+
+        r = requests.get(url, params=params)
+
+        return r.json()

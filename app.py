39ac7eec89d3579bd964f1546f8c3427cb34f575
Saurabh Sood <saurabhsood91@gmail.com>
2017-03-31 16:45:20 -0600

    basic object session handling

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ from lib.github import Github
 from datetime import datetime
 import requests
 import urllib
+import json
 import re
 
 app = Flask(__name__)
 ask = Ask(app, "/alexa")
+
+
+def _get_job_object(job):
+    return {
+        'company': job['company'],
+        'title': job['title'],
+        'how_to_apply': job['how_to_apply'],
+        'url': job['url']
+    }
+
 
 @ask.launch
 def new_ask():
@@ -16,12 +27,55 @@ def new_ask():
     return question(welcome) \
         .reprompt(reprompt)
 
-
 @ask.intent('SearchJobsIntent')
-def search_jobs(role):
-    jobs = Github.get_jobs(query=role)
-    response = render_template('search_job_response', number=len(jobs))
-    return statement(response)
+def search_jobs(role, city):
+    jobs = Github.get_jobs(query=role, location=city)
+
+    if len(jobs) == 0:
+        if city:
+            response = render_template(
+                'no_jobs_found_city',
+                role=role,
+                city=city
+            )
+            return statement(response)
+        else:
+            response = render_template(
+                'no_jobs_found',
+                role=role
+            )
+            return response
+    response = render_template('search_job_response')
+
+    job_objects = map(_get_job_object, jobs)
+
+    session.attributes['jobs'] = job_objects
+    session.attributes['index'] = 0
+    return question(response)
+
+
+@ask.intent('NextIntent')
+def next_job():
+    # assuming we have saved jobs
+    # TODO: handle case where we do not have jobs
+    current_job = session.attributes['jobs'][session.attributes['index']]
+    # increment job index
+    session.attributes['index'] += 1
+
+    role = current_job['title']
+    company = current_job['company']
+
+    return statement(
+        render_template(
+            'job_listing',
+            role=title,
+            company=company
+        )
+    )
+
+
+# @ask.intent('YesIntent'):
+# def yes_intent()
 
 if __name__ == '__main__':
     app.run(debug=True)
